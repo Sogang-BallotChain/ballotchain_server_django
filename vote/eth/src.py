5ae1c0126d3adaf0331848572a6188a3638a164b
omnipede <omnipede@naver.com>
2019-11-12 00:36:31 +0900

    솔리디티 코드 수정
    endBallot 으로 직접 투표 종료하도록 수정

--- vote/eth/src.py
+++ vote/eth/src.py
@@ -20,21 +20,20 @@ contract Ballot {
     Candidate winner;
     uint8 winner_index;
     
+    address admin;
+    bool is_ended;
+    
     constructor (uint8 _numCandidates, uint _start_time, uint _end_time) public {
+        admin = msg.sender;
         candidates.length = _numCandidates;
         start_time = _start_time;
         end_time = _end_time;
     }
     
-    modifier onlyAfterEnd {
-        require (now >= end_time, "Ballot is not finished");
-        _;
-    }
-    
     function vote (uint8 toCandidate) public {
         Voter storage voter = voters[msg.sender];
         require (!voter.voted && toCandidate <= candidates.length, "Invalid voting.");
-        require (now < end_time, "Ballot is finished");
+        require(!is_ended);
         
         voter.voted = true;
         voter.vote = toCandidate;
@@ -46,17 +45,28 @@ contract Ballot {
         }
     }
     
+    function endBallot () public {
+        require (msg.sender == admin);
+        is_ended = true;
+    }
     
-    function showWinner () onlyAfterEnd public view returns (uint8) {
+    function showWinner () public view returns (uint8) {
+        require (is_ended);
         return winner_index;
     }
     
-    function showVoteCount (uint8 candidate) onlyAfterEnd public view returns (uint8 voteCount) {
+    function showVoteCount (uint8 candidate) public view returns (uint8 voteCount) {
+        require(is_ended);
         voteCount = candidates[candidate].voteCount;
     }
     
-    function showVote () onlyAfterEnd public view returns (uint8 voteTo) {
-        voteTo = voters[msg.sender].vote;
+    function showVote (address voter) public view returns (uint8 voteTo) {
+        require(is_ended);
+        voteTo = voters[voter].vote;
+    }
+    
+    function isEnded () public view returns (bool) {
+        return is_ended;
     }
 }
 '''
