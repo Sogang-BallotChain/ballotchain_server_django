4d56c7e9df3c6d9f9e5e20e9bd9b9a9e75f0c82d
omnipede <omnipede@naver.com>
2019-11-17 15:35:55 +0900

    Geth config 추가

--- vote/eth/config.py
+++ vote/eth/config.py
@@ -1,6 +1,11 @@
+
+# Ropsten configuration
 rpc_url = "https://ropsten.infura.io/v3/49b9acbd693940a0bf84fef21253e244"
-#rpc_url = "http://www.ballotchain.net:8805"
 master = "21DF8E8466D4C5B11BE3E1890C45C99A290BC3D7388151CC658BC35885D50F74"
 
-geth_master="0x9442103789183D14A7F5537758917a19B0375dD6"
-geth_password="1234566"
+
+# Geth configuration
+'''
+rpc_url = "http://www.ballotchain.net:8805"
+master = "0x41c5c3326bb54c3fad0192672805f8e69d8d368c1545805655935d58a0db497e"
+'''
--- vote/eth/test.py
+++ vote/eth/test.py
@@ -1,5 +1,43 @@
-from . import interface, config
+from . import config
+from .interface import Deployer, BallotContract
+from eth_account import Account
 from web3 import Web3, HTTPProvider
 
 w3 = Web3(HTTPProvider(config.rpc_url))
 
+account = w3.eth.account.create("1234566")
+pub_key = account.address
+prv_key = account.privateKey.hex()
+
+def getGasFee (_to):
+    
+    master_pub_key = "0x55969a2b4d94684036fa0948468B8C1D54f15FC2"
+    master_prv_key = "0x41c5c3326bb54c3fad0192672805f8e69d8d368c1545805655935d58a0db497e"
+    
+    w3 = Web3(HTTPProvider(config.rpc_url))
+    account = Account().privateKeyToAccount(master_prv_key)
+
+    signed_txn = account.signTransaction({
+        'nonce': w3.eth.getTransactionCount(account.address),
+        'gas': 21000,
+        'gasPrice': w3.toWei('20', 'gwei'),
+        'to': _to,
+        'value': w3.toWei('65952900', 'gwei')
+    })
+
+    tx_hash = w3.eth.sendRawTransaction(signed_txn.rawTransaction)
+    tx_receipt = w3.eth.waitForTransactionReceipt(tx_hash)
+
+print(pub_key, prv_key)
+getGasFee(pub_key)
+
+deployer = Deployer(16, 100, 120)
+addr = deployer.deploy("0x41c5c3326bb54c3fad0192672805f8e69d8d368c1545805655935d58a0db497e")
+
+ballotContract = BallotContract(addr, prv_key)
+ballotContract.vote(7)
+
+ballotContract = BallotContract (addr, "0x41c5c3326bb54c3fad0192672805f8e69d8d368c1545805655935d58a0db497e")
+ballotContract.endBallot()
+x = ballotContract.getWinner()
+print(x)
